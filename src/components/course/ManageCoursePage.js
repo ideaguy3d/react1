cf2514f84bbc5f6a4820250964b0a48babde4128
Julius Alvarado <angularjs.uiux@gmail.com>
2017-07-02 12:40:12 -0700

    finished writing the jsx and comps for course forms

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Julius Alvarado on 7/2/2017.
+ */
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Manage Course</h1>
+            </div>
+        );
+    }
+}
+ManageCoursePage.propTypes = {};
+function mapStateToProps(state, ownProps) {
+    return {
+        state: state
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage); 
--- src/routes.js
+++ src/routes.js
@@ -9,12 +9,15 @@ import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
 import GamePage from './components/game/GamePage';
+import ManageCoursePage from './components/course/ManageCoursePage'
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="about" component={AboutPage}/>
         <Route path="courses" component={CoursesPage}/>
+        <Route path="course" component={ManageCoursePage}/>
+        <Route path="course/:id" component={ManageCoursePage}/>
         <Route path="game" component={GamePage}/>
     </Route>
 );
